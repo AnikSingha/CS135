d27552c838fbd977fab31b7428c6c03f74f0b090
= <dcnation1000000@gmail.com>
2022-05-01 21:42:40 -0400

    start project 3

--- Project3/Board.cpp
+++ Project3/Board.cpp
@@ -1,41 +1,68 @@
-class Board{
-    int numRows, numCols;
-    int *panel[numRows][numCols];
-    int max = 0, target = 32;
-    Board(){
-        numRows = 3;
-        numCols = 3;
-        int *panel[numRows][numCols];
-    }
+#include <iostream>
+#include <iomanip>
+#include "Board.hpp"
 
-    Board(int m){
-        if (m >= 1){
-            numRows = m;
-            numCols = m;
-        } else{
-            numRows = 3;
-            numCols = 3;
+using namespace std;
+
+Board::Board(){
+    max = 0; 
+    target = 32;
+    numRows = 3; 
+    numCols = 3;
+    panel[numRows][numCols];
+}
+
+Board::Board(int m){
+    max = 0; 
+    target = 32;
+    if(m >= 1){ 
+        numRows = m; 
+        numCols = m; 
+    } else { 
+        numRows = 3; 
+        numCols = 3; 
+    }
+    panel[numRows][numCols];
+}
+
+Board::Board(int m, int n){
+    max = 0; 
+    target = 32;
+    if(m >= 1 && n >= 1){ 
+        numRows = m; 
+        numCols = n; 
+    } else {
+        numRows = 3; 
+        numCols = 3; 
+    }
+    panel[numRows][numCols];
+}
+
+Board::~Board(){
+    for(int i = 0; i < sizeof(panel) / sizeof(panel[0]); i++){
+        delete[] panel[i];
+    }
+    delete[] panel;
+    panel = nullptr;
+}
+
+void Board::print() const{
+    for(int row = 0; row < numRows; row++){
+        for (int i = 0; i < numCols; i++){
+            cout << "+----";
         }
-        panel[numRows][numCols];
-    }
+        
+        cout << "+" << endl;
 
-    Board(int m, int n){
-        if (m >= 1 && n >= 1){
-            numRows = m;
-            numCols = n;
-        } else{
-            numRows = 3;
-            numCols = 3;
+        for(int col = 0; col <= numCols; col++){
+            if(col != numCols - 1){    
+                cout << "|    ";
+            }
         }
-        int panel[numRows][numCols];
+        cout << "|" << endl;
     }
-    
-    ~Board(){
-        delete[] panel;
+    for (int j = 0; j < numCols; j++){
+            cout << "+----";
     }
-
-    void print(){
-
-    }
-
-};
+    cout << "+" << endl;
+}
